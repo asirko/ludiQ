70cb5fee555a002ab1854b26245b23a2ee14d579
asirko <alexandre@jump-biz.com>
2018-03-10 15:19:14 +0100

    added a first data set

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', function (req, res) {
+  res.send({
+    hello: 'world!'
+  });
 });
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
